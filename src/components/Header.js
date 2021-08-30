d7968ba1e07fc2824245fe283902f86801baafb9
Dhruv <dhruvsakariya2304@gmail.com>
2021-08-30 19:02:50 +0530

    Create a React app and publish it to GitHub Pages

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <h2>Expense Tracker <sub>dhruv sakariya</sub></h2>;
+};
+
+export default Header;
---
+++ src/context/GlobalState.js
@@ -0,0 +1,46 @@
+import React, { useReducer, createContext } from "react";
+import AppReducer from "./AppReducer";
+
+const initialState = {
+  transactions: [
+    { id: 1, text: "Flower", amount: -20 },
+    { id: 2, text: "Salary", amount: 300 },
+    { id: 3, text: "Book", amount: -10 },
+    { id: 4, text: "Camera", amount: 150 },
+  ],
+};
+
+export const GlobalContext = createContext(initialState); // Context api
+
+const GlobalState = (props) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState); // Reducer function
+
+  // Actions
+
+  const deleteTransaction = (id) => {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  };
+
+  const addTransaction = (transaction) => {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  };
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
+      {props.children}
+    </GlobalContext.Provider>
+  );
+};
+export default GlobalState;
